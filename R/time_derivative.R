663eaf5b67473381540352bd4fefd0483c6be2e8
sodium-hydroxide <43483259+sodium-hydroxide@users.noreply.github.com>
2024-03-22 12:35:53 -0600

    Edit time_derivative.R

--- R/time_derivative.R
+++ R/time_derivative.R
@@ -1,7 +1,19 @@
+#' Title
+#'
+#' @param data 
+#' @param column 
+#' @param order_by 
+#' @param time_step 
+#' @param logarithmic 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 time_derivative <- function(
         data,
         column = "",
-        order_by = "",
+        order_by = NULL,
         time_step = 1.0,
         logarithmic = FALSE) {
 
@@ -11,15 +23,36 @@ time_derivative <- function(
         devtools::install_github("sodium-hydroxide/btools")
     }
 
-    .data. <- btools::shift(
+    data <- btools::shift(
         data = data,
-        column = column,
+        column = "value",
         order_by = order_by
     )
+
+    .data. <- data
+    .data.$value <- data[[column]]
+
+    .data.$derivative <-
+        (.data.$value_1_back - .data.$value) / time_step
+    .data.$derivative[nrow(.data.)] <- 0
     
-    data[[paste(column, "_dot", sep = "")]] <-
-        (.data.[[paste(column, "_1_back", sep = "")]] - .data.[[column]]) /
-        time_step
-    
-    
+    if (logarithmic) {
+        .data.$mean <- 
+            (.data.$value_1_back - .data.$value) * 0.5
+        
+        .data.$zero_over_zero <- 
+            .data.$mean == 0 &
+            .data.$derivative == 0
+        
+        .data.$log_derivative <- .data.$derivative / .data.$mean
+        
+        .data.$log_derivative[.data.$zero_over_zero] <- 0
+        
+        data[[paste(column, "log_dot")]] <- .data.$log_derivative
+    }
+    else {
+        data[[paste(column, "dot")]]
+    }
+
+    return(data)
 }
